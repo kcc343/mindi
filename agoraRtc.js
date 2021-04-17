600076ab7354ea35335ab41e3f534994b90244e0
kcc343 <kcc343@uw.edu>
2021-04-16 21:55:34 -0700

    leave host

--- agoraRtc.js
+++ agoraRtc.js
@@ -103,8 +103,7 @@ function addLeaveHost() {
     localStreams.camera.stream.stop();
     localStreams.screen.stream.stop();
     let paneHost = document.getElementById("right-pane");
-    console.log(paneHost.firstChild)
-    paneHost.remove(paneHost.firstChild);
+    paneHost.remove(paneHost.firstChild.nextSibling);
     document.getElementById("remoteStreams").innerHTML = "";
     document.getElementById("HostStream").innerHTML = "";
     document.getElementById("ScreenStream").innerHTML = "";
